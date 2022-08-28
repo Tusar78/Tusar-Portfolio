c55eedbf5e3cb999ba39b325375c33a1d24d17fe
Tusar <tusarahmed141@gmail.com>
2022-08-28 20:06:09 +0600

    Skill section functionality done

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,23 @@ const linkAction = () => {
 
 navLinks.forEach((link) => link.addEventListener("click", linkAction));
 
+/*==== Accordion design upon skills ====*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  let skillsContentLength = skillsContent.length;
+
+  for (let i = 0; i < skillsContentLength; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((element) => {
+  element.addEventListener("click", toggleSkills);
+});
