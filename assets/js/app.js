571fc6a87cec18705ecd4633aee8df39ff3e5f41
Tusar <tusarahmed141@gmail.com>
2022-08-28 21:06:41 +0600

    Qualification functionality added

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,3 +51,25 @@ function toggleSkills() {
 skillsHeader.forEach((element) => {
   element.addEventListener("click", toggleSkills);
 });
+
+/*==== Qualifications Tab ====*/
+const tabs = document.querySelectorAll("[data-target]");
+tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+
+    tab.classList.add("qualification__active");
+  });
+});
