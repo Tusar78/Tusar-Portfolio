9eacc45cc6586668ce02411b3c5c9bf9f9d2307c
Tusar <tusarahmed141@gmail.com>
2022-08-29 01:00:22 +0600

    Scrool up button done

--- assets/js/app.js
+++ assets/js/app.js
@@ -110,3 +110,51 @@ let swiperPortfolio = new Swiper(".portfolio__container", {
     clickable: true,
   },
 });
+
+/*==== Scroll section active link ====*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*==== Change background header ====*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) {
+    nav.classList.add("scroll-header");
+  } else {
+    nav.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==== Shoe scroll up button ====*/
+function scrollUp() {
+  const scrollTUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) {
+    scrollTUp.classList.add("show-scroll");
+  } else {
+    scrollTUp.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollUp);
