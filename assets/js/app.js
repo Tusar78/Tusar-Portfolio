419077330a2ea964a4c29170cc20e69819d905f0
Tusar <tusarahmed141@gmail.com>
2022-08-29 00:25:25 +0600

    Portfolio section functionality done

--- assets/js/app.js
+++ assets/js/app.js
@@ -96,3 +96,17 @@ modalCloses.forEach((modalClose) => {
     });
   });
 });
+
+/*==== Portfolio swiper ====*/
+let swiperPortfolio = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
