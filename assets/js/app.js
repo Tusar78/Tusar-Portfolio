12c48e0e2c63c024a1d3ba13e530c13b53539c12
Tusar <tusarahmed141@gmail.com>
2022-08-28 23:21:15 +0600

    Service functionality added

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,3 +73,26 @@ tabs.forEach((tab) => {
     tab.classList.add("qualification__active");
   });
 });
+
+/*==== Service Modal ====*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalButtons = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".service__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalButtons.forEach((modalButton, i) => {
+  modalButton.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
